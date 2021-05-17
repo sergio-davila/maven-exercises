c6a5e3d94e8e91c3314637277b885939106685ad
Sergio Davila <sergio.alexander.davila15@gmail.com>
2021-05-17 10:25:15 -0500

    feat: refactored a class

---
+++ src/main/MavenExercise1.java
@@ -0,0 +1,13 @@
+package main;
+
+import java.util.Scanner;
+
+public class MavenExercise1 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Please input anything!");
+
+    }
+}
